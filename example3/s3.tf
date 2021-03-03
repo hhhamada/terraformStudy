d53119ab2f1220f1a6eaad5dea95853df32aea1d
hhhamada <hmdhrk.71@gmail.com>
2021-03-03 09:07:34 +0000

    before NAT gateway

---
+++ example3/s3.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "alb_log" {
+    bucket = "alb-log-terraformStudy-hamada"
+    
+    force_destroy = true
+    
+    lifecycle_rule {
+        enabled = true
+        
+        expiration {
+            days = "180"
+        }
+    }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+    bucket = aws_s3_bucket.alb_log.id
+    policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+    statement {
+        effect    = "Allow"
+        actions   = ["s3:PutObject"]
+        resources = ["awn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+        
+        principals {
+            type        = "AWS"
+            identifiers = ["433702354322"]
+        }
+    }
+}
