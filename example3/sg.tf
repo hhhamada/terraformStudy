36f6798098afeb9739c6338a417755485a13eb3c
hhhamada <hmdhrk.71@gmail.com>
2021-03-12 09:23:46 +0000

    a

--- example3/sg.tf
+++ example3/sg.tf
@@ -2,6 +2,30 @@ module "example_sg" {
     source = "./security_group"
     name   = "module_sg"
     vpc_id = aws_vpc.example.id
-    port   = "80"
+    port   = 80
     cidr_block = ["0.0.0.0/0"]
+}
+
+module "http_sg" {
+    source     = "./security_group"
+    name       = "http-sg"
+    vpc_id     = "aws_vpc.example.id"
+    port       = 80
+    cidr_block = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+    source     = "./security_group"
+    name       = "https-sg"
+    vpc_id     = "aws_vpc.example.id"
+    port       = 443
+    cidr_block = ["0.0.0.0/0"]
+}
+    
+module "http_redirect_sg" {
+    source     = "./security_group"
+    name       = "https-sg"
+    vpc_id     = "aws_vpc.example.id"
+    port       = 8080
+    cidr_block = ["0.0.0.0/0]
 }
